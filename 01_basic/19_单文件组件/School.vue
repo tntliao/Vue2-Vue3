711d315b7f76ad38eaf6a280087b7a5ac929ab4c
ljj <786602661@qq.com>
2021-08-26 18:02:38 +0800

    8-26

---
+++ 01_basic/19_单文件组件/School.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h2>{{ address }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "sgg",
+      address: "bj",
+    };
+  },
+};
+</script>
+<style>
+</style>
---
+++ 01_basic/19_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <h3>{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "ljj",
+      age: 17,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
